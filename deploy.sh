de093dd767fc142f09bb10b72201b6dde6a6cacd
Alba Hoo <alba@vivi.io>
2018-01-28 06:18:54 +1100

    Fix bug in deploy shell

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,14 @@ touch _site/.nojekyll
 
 echo ".sass-cache\n.jekyll-metadata" > .gitignore
 git add _site && git commit -m "Initial _site subtree commit"
-git reset HEAD~
-
-echo "_site\n.sass-cache\n.jekyll-metadata" > .gitignore
 
 # delete remote-pages
 git push origin --delete gh-pages
 
 git subtree push --prefix _site origin gh-pages
+
+git reset HEAD~
+echo "_site\n.sass-cache\n.jekyll-metadata" > .gitignore
 
 echo 'Deploy to github'
 
